d8af9e87d1774944b2a74fa18052d2f643257e77
unknown <gulzamanarain77@gmail.com>
2025-08-16 23:21:03 +0500

    first commit

---
+++ js/logical.js
@@ -0,0 +1,17 @@
+let a = 10
+let b = 20
+let c = 25
+
+let result = (a > b) && (b < c)
+
+// document.getElementById("result").innerHTML = result
+
+
+
+let num1 = 45
+let num2 = 54
+
+let ans = num1 < num2 || num1 > num2
+
+document.getElementById("result").innerHTML = ans
+
---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let a = prompt("Enter a");
+// let b = 5;
+// let result = a++ + b + ++a + a + --b + a++;
+// document.getElementById("para").innerText = "result: " + result;
+
+
+let num1 = +prompt("enter your first number")
+let num2 = +prompt("enter your second number")
+
+let result = num1 + num2;
+document.getElementById("para").innerText = result;
