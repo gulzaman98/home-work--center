e3b6005d02f683630ebc374c41ddffe1fb6c9ce6
unknown <gulzamanarain77@gmail.com>
2025-08-23 02:09:29 +0500

    done

--- js/if-else.js
+++ js/if-else.js
@@ -79,20 +79,96 @@
 
 
 
-let name = prompt("Enter your name")
-let email = prompt("Enter your email")
-let password = prompt(123456)
-let confirmPassword = prompt(123456)
+// let name = prompt("Enter your name")
+// let email = prompt("Enter your email")
+// let password = prompt(123456)
+// let confirmPassword = prompt(123456)
 
-if(name == "gulzaman" &&
-    email == "gulzamanarain@gmail.com" &&
-    password == 123456 && 
-    confirmPassword == password
-)
-{
-    document.write("loged in successfully")
+// if(name == "gulzaman" &&
+//     email == "gulzamanarain@gmail.com" &&
+//     password == 123456 && 
+//     confirmPassword == password
+// )
+// {
+//     document.write("loged in successfully")
+// }
+
+// else{
+//     document.write("something went rong")
+
+
+
+
+
+
+// let num = +prompt("Enter your number")
+
+// if(num >=10){
+//     document.write("number is greater than 10")
+// }
+
+// else{
+//     document.write("number is less than 10")
+// }
+
+
+
+
+// let num = +prompt("Enter your number")
+
+// if(num % 2 === 0){
+//     document.write("number is even")
+// }
+
+// else{
+//     document.write("number is odd")
+// }
+
+
+// let marks = +prompt("Enter your marks")
+
+// if (marks >= 90){
+//     document.write("Grade A+")
+// }
+// else if(marks >= 80){
+//     document.write("Grade A")
+// }
+
+// else if(marks >= 70){
+//     document.write("Grade B")
+// }
+// else{
+//     document.write("Fail")
+// }
+
+// let num = +prompt("enter your number")
+
+// if(num > 0){
+//     document.write("Positive number")
+// }
+// else if(num < 0){
+//     document.write("Negative number")
+// }
+// else if(num == 0){
+//     document.write("Zero number ")
+// }
+
+let num1 = +prompt("Enter your first number ")
+let num2 = +prompt("Enter your second number")
+let num3 = + prompt("Enter your third number")
+
+if(num1 > num2 && num1 > num3){
+    document.write("Number 1 is greater")
+}
+
+else if(num2 > num3 && num2 > num1){
+    document.write("Number 2 is greater")
+}
+
+else if(num3 > num2 && num3 > num1){
+    document.write("Number 3 is greater")
 }
 
 else{
-    document.write("something went rong")
+    document.write("All numbers are equal")
 }
---
+++ js/switch.js
@@ -0,0 +1,68 @@
+// let day= +prompt("Enter your day") 
+
+// switch (day) {
+//     case 1:
+//         document.write(" <h2 class='monday'> Monday")
+//         break;
+
+//     case 2:
+
+//         document.write(" <h2 class='tuesday'>Tuesday")
+//         break;
+
+//     case 3:
+
+//         document.write(" <h2 class='wednesday'>Wednesday")
+//         break;
+
+//     case 4:
+
+//         document.write(" <h2 class='thursday'>Thursday")
+//         break;
+
+//     case 5:
+
+//         document.write(" <h2 class='friday'>Friday")
+//         break;
+
+//     case 6:
+
+//         document.write(" <h2 class='saturday'>Saturday")
+//         break;
+
+//         case 7:
+
+//         document.write(" <h2 class='sunday'>Sunday")
+//         break;
+
+//     default:
+
+//         document.write(" <h2 class='invalid'>Invalid day")
+
+
+// // }
+
+// let group = +prompt("Enter your age group number (1=child , 2=teenager , 3=adult , 4=senior)")
+// switch (group){
+//     case 1:
+//         document.write("<h2 class=monday> Ticket Price: 200 PKR (Child age)")
+//         break;
+
+//         case 2:
+//             document.write("<h2 class=tuesday> Ticket price : 500 PKR (Teen Ager)")
+//             break;
+
+//             case 3:
+//                 document.write("<h2 class=wednesday> Ticket price : 1000 PKR (Adult)")
+//                 break;
+//                 case 4:
+//                     document.write("<h2 class=thursday> Ticket price : 1200 PKR ()Senior")
+//                     break;
+
+//                     default:
+//                         document.write(" <h2 class=friday> Invalid Age Number")
+// }
+    
+
+
+
