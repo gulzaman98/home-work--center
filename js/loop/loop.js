1c97e41a56e5af799e430ab36974f23c6f5a299b
unknown <gulzamanarain77@gmail.com>
2025-08-22 23:02:52 -0700

    done

---
+++ js/loop/loop.js
@@ -0,0 +1,7 @@
+for(let i = 1 ; i <= 10; i++){
+    document.write( i ,"This is for loop <br>")
+}
+
+// for(let i = 10 ; i <= 10; i--){
+//     document.write( i ,"This is for loop")
+// }
---
+++ js/switch/switch.js
@@ -0,0 +1,25 @@
+let vowels = prompt("Enter your vowels")
+
+switch(vowels){
+    case "a":
+        document.write("vowels")
+        break;
+         case "e":
+        document.write("vowels")
+        break;
+         case "i":
+        document.write("vowels")
+        break;
+         case "o":
+        document.write("vowels")
+        break;
+         case "u":
+        document.write("vowels")
+        break;
+
+
+     
+     default: 
+     document.write("consonant")  
+}
+
