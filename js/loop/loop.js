bb0d7fc7292fc3c6e4980493e526fba418f09801
unknown <gulzamanarain77@gmail.com>
2025-08-25 23:05:43 -0700

    done

--- js/loop/loop.js
+++ js/loop/loop.js
@@ -1,7 +1,24 @@
-for(let i = 1 ; i <= 10; i++){
-    document.write( i ,"This is for loop <br>")
-}
+// for(let i = 1 ; i <= 10; i++){
+//     document.write( i ,"This is for loop <br>")
+// }
 
 // for(let i = 10 ; i <= 10; i--){
 //     document.write( i ,"This is for loop")
 // }
+
+
+
+// Do while loop
+
+let a = 1
+do{
+    if(a % 2 != 0){
+        document.write( a ,"<br>")
+    }
+    // document.write( a ," : This is a do while loop <br>")
+    a++
+}while(a <= 10)
+
+
+
+
