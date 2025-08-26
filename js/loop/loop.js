5c86dd1e829c9761dafef7c47e26cf98f33ee4a4
unknown <gulzamanarain77@gmail.com>
2025-08-26 22:15:57 +0500

    loop folder add

---
+++ js/loop/loop.js
@@ -0,0 +1,53 @@
+// for(let i = 1 ; i <= 10; i++){
+//     document.write( i , ":This is for loop <br>")
+// }
+
+// for(let i = 1 ; i <= 10; i++){
+//     document.write( i ," : This is for loop <br>")
+// }
+
+// for(let i = 1; i <= 5 ; i++){
+//     document.write("hello : " , i , "<br>")
+// }
+
+//  for (let i = 1; i <=10 ; i ++){
+//     document.write("11" , " x ", i , " = ",i*11 , "<br>")
+// }
+
+// WHILE LOOP
+
+
+// let i = 1;
+// while(i <= 5){
+//     document.write(i , "<br>")
+//     i++
+// }
+
+
+// DO WHILE LOOP 
+
+let i = 6;
+do{
+    document.write(i , "<br>")
+}while(i <=5)
+
+//     document.write( i ,"This is for loop <br>")
+// }
+
+// for(let i = 10 ; i <= 10; i--){
+//     document.write( i ,"This is for loop")
+// }
+
+
+
+// Do while loop
+
+let a = 1
+do{
+    if(a % 2 != 0){
+        document.write( a ,"<br>")
+    }
+    // document.write( a ," : This is a do while loop <br>")
+    a++
+}while(a <= 10)
+
