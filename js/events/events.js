1cbdd824515fd5f2ac91b36806607624a493dd03
unknown <gulzamanarain77@gmail.com>
2025-09-03 22:59:01 -0700

    done

--- js/events/events.js
+++ js/events/events.js
@@ -1,1 +1,29 @@
 let myapp = (nam1 ,nam2)=>document.write(nam1+nam2)
+
+
+
+btn.addEventListener("click" , ()=>{
+    console.log("phla listener run hua");
+    document.getElementById("para").innerHTML = "Hello world"
+})
+
+btn.addEventListener("click" , function(){
+    console.log("dusra listener run hua");
+    document.getElementById("para").style.color = "red"
+})
+
+
+btn.onclick = ()=>document.getElementById("para").innerHTML = "You Click the button"
+btn.onmouseover = ()=>document.getElementById("para").style . color = "red"
+
+
+
+window.onresize=()=>{
+    console.log("Window is resize")
+}
+
+window.onload=()=>{
+    alert("Welcome gulzaman arain")
+    console.log("welcome gulzaman")
+}
+
---
+++ js/object/object.js
@@ -0,0 +1,8 @@
+let student = {
+    Id: 44102,
+    Name: 'Gulzaman',
+    Email:"gulzamanarain77@gmail.com",
+    Age: 20
+}
+
+document.write(student["Name"] , "<br>" , [Age])
