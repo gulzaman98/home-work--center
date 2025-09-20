c7a0a3f02cfd3cae90fb939fd90c9188fb50063b
unknown <gulzamanarain77@gmail.com>
2025-09-19 22:54:36 -0700

    done

---
+++ js/Dom2/main.js
@@ -0,0 +1,13 @@
+// let para = document.getElementsByClassName("para")
+// para[0].innerHTML = "Hello World"
+// para[1].style.color = "red"
+
+
+
+let para = document.getElementsByName(`p`)
+para[0].innerHTML = "Hello World"
+para[1].style.color = "red"
+
+
+
+
