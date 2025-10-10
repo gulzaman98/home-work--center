4710d34c6a3e7c6aead54a6ce50ba6da7ca9db52
unknown <gulzamanarain77@gmail.com>
2025-10-11 00:34:11 +0500

    done

--- js/switch/switch.js
+++ js/switch/switch.js
@@ -1,26 +1,26 @@
 let vowel = prompt("Enter your Alphabet");
 
-switch(vowel){
-    case "a":
-        document.write("vowel")
-        break;
-         case "e":
-        document.write("vowel")
-        break;
-      
-         case "i":
-        document.write("vowel")
-        break;
-      
-         case "o":
-        document.write("vowel")
-        break;
-      
-         case "u":
-        document.write("vowel")
-        break;
-      default:
-        document.write("consonant")
-        break;
-      
+switch (vowel) {
+  case "a":
+    document.write("vowel")
+    break;
+  case "e":
+    document.write("vowel")
+    break;
+
+  case "i":
+    document.write("vowel")
+    break;
+
+  case "o":
+    document.write("vowel")
+    break;
+
+  case "u":
+    document.write("vowel")
+    break;
+  default:
+    document.write("consonant")
+    break;
+
 }
