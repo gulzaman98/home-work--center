a7ac275b0c9548a7f1112a60cd89c30c2e30c591
STUDENT <std1@LABSFC.COM>
2025-10-17 22:57:28 -0700

    done

---
+++ Jquery/class-4/index-4.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+
+    // NAME VALIDATION
+
+    $("#name").keyup(function(){
+        let name = $(this).val()
+        let nameRe = /^[a-zA-Z\s]{3,}$/
+
+        if(!nameRe.test(name)){
+            $(this).next("span").show().text("Only Allow Alphabates").css("color","red")
+            $(this).css("border","2px solid red")
+        }else{
+            $(this).next("span").hide()
+            $(this).css("border", "2px solid green")
+        }
+    })
+
+    // Email
+
+    
+    $("#email").keyup(function(){
+        let name = $(this).val()
+        let emailRe = /^[a-z0-9\]
+
+        if(!emailRe.test(name)){
+            $(this).next("span").show().text("Only Allow Alphabates").css("color","red")
+            $(this).css("border","2px solid red")
+        }else{
+            $(this).next("span").hide()
+            $(this).css("border", "2px solid green")
+        }
+    })
+})
